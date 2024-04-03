dc82c3bf6c288fa51500070224f375b4610109a7
hyub <hyub@hyubui-MacBookPro.local>
2024-04-03 11:43:59 +0900

    aws delete

--- src/server/server.js
+++ src/server/server.js
@@ -2,9 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-// 'dist' 디렉토리에 있는 파일을 정적 파일로 제공
-app.use(express.static("dist"));
-
+app.use(express.static(path.join(__dirname, "..", "..", "dist")));
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "..", "..", "dist", "index.html"));
 });
